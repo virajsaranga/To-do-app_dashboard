534534d1e2d56e5199c0bf2d51213f247491fdba
virajsaranga <virajridi@gmail.com>
2024-05-31 18:51:33 +0530

    responsive fixed

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,21 @@
-import React from "react";
-import "../styles/Header.css";
-import profil_pic from "../assets/profile.png";
-import bell_icon from "../assets/bell-icon.png";
-import icon_down from "../assets/Icon.png";
+
+import React from 'react';
+import '../styles/Header.css';
+import profil_pic from '../assets/profile.png';
+import bell_icon from '../assets/bell-icon.png';
+import icon_down from '../assets/Icon.png'
+
 
 const Header = () => {
   return (
     <header className="header">
-      <h1 className="header-text">Dashboard</h1>
+      <h1 className='header-text'>Dashboard</h1>
       <div className="user-info">
+        
         <i className="fa fa-bell"></i>
-        <img src={bell_icon} alt="User" className="bell-image" />
-        <img src={profil_pic} alt="User" className="user-image" />
-        <img src={icon_down} alt="User" className="down-image" />
+        <img src={bell_icon } alt="User" className="bell-image"/>
+        <img src={profil_pic } alt="User" className="user-image"/>
+        <img src={icon_down } alt="User" className="down-image"/>
       </div>
     </header>
   );
